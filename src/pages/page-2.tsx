d8da624a372211e1264bed7509c42fab605e36fc
Evan Lie <evan.taurusta@gmail.com>
2020-05-26 15:05:29 -0700

    adding space

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -10,7 +10,7 @@ const SecondPage = (props: PageProps) => (
     <SEO title='Page two' />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2 ({props.path})</p>
-    <Link to='/'>Go back to the homepage</Link>
+    <Link to='/'>Go back to the homepage </Link>
   </Layout>
 )
 
