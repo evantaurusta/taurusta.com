787af224f4cabc6c0e125e32166077859876054e
Evan Lee <evantaurusta@users.noreply.github.com>
2020-06-17 16:36:11 -0700

    Header component (#10)
    * Remove unecessary code
    
    * initial card component
    
    * initial card component
    
    * fix proptypes declaraiton in Movie page
    
    * fix proptypes declaraiton in Movie page
    
    * update readme
    
    * update readme
    
    * initial commit

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+// eslint-disable-next-line no-unused-vars
+import c from './card.scss'
 
 const Card = ({ company, description }) => {
   return (
