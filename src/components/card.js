cd7869da3879405c7fc36f494ca8a24bb61447fe
Evan Lee <evantaurusta@users.noreply.github.com>
2020-06-18 14:26:57 -0700

    Install styled components (#12)
    * Remove unecessary code
    
    * initial card component
    
    * initial card component
    
    * fix proptypes declaraiton in Movie page
    
    * fix proptypes declaraiton in Movie page
    
    * update readme
    
    * update readme
    
    * initial commit
    
    * remove sass
    
    * remove sass
    
    * fix build
    
    * added first styled components

--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const h2 = styled.h2`
+  background-color: grey;
+`
 
 const Card = ({ company, description }) => {
   return (
