f382ee73ed9e1f866a764db9332cf41eec69e877
Evan Lee <evantaurusta@users.noreply.github.com>
2020-06-20 11:01:13 -0700

    Install styled components (#13)
    * Remove unecessary code
    
    * initial card component
    
    * initial card component
    
    * fix proptypes declaraiton in Movie page
    
    * fix proptypes declaraiton in Movie page
    
    * update readme
    
    * update readme
    
    * initial commit
    
    * remove sass
    
    * remove sass
    
    * fix build
    
    * added first styled components
    
    * convert header to styled component
    
    * fix merge conflicts

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,34 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
+import styled from 'styled-components'
+
+const HeaderWrapper = styled.header`
+  background-color: slategrey;
+  margin-bottom: 1.45rem;
+`
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+const Heading = styled.h1`
+  margin: 0;
+`
+
+const HeadingLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      backgroundColor: 'slategrey',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1>
-        <Link
-          to='/'
-          style={{
-            color: 'blue',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <HeaderWrapper>
+    <HeaderContainer>
+      <Heading>
+        <HeadingLink to='/'>{siteTitle}</HeadingLink>
+      </Heading>
+    </HeaderContainer>
+  </HeaderWrapper>
 )
 
 Header.propTypes = {
